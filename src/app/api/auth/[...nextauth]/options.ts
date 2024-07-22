1d32ce2b8221985e8699c5fd821f9530319b4552
shivang4857 <shivangd801@gmail.com>
2024-07-22 12:54:04 +0530

    unique_username

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -5,69 +5,69 @@ import dbConnect from '@/app/lib/dbConnect';
 import UserModel from '@/app/model/User';
 
 export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      id: 'credentials',
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'Email', type: 'text' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials: any): Promise<any> {
-        await dbConnect();
-        try {
-          const user = await UserModel.findOne({
-            $or: [
-              { email: credentials.identifier },
-              { username: credentials.identifier },
-            ],
-          });
-          if (!user) {
-            throw new Error('No user found with this email');
+    providers: [
+      CredentialsProvider({
+        id: 'credentials',
+        name: 'Credentials',
+        credentials: {
+          email: { label: 'Email', type: 'text' },
+          password: { label: 'Password', type: 'password' },
+        },
+        async authorize(credentials: any): Promise<any> {
+          await dbConnect();
+          try {
+            const user = await UserModel.findOne({
+              $or: [
+                { email: credentials.identifier },
+                { username: credentials.identifier },
+              ],
+            });
+            if (!user) {
+              throw new Error('No user found with this email');
+            }
+            if (!user.isVerified) {
+              throw new Error('Please verify your account before logging in');
+            }
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+            if (isPasswordCorrect) {
+              return user;
+            } else {
+              throw new Error('Incorrect password');
+            }
+          } catch (err: any) {
+            throw new Error(err);
           }
-          if (!user.isVerified) {
-            throw new Error('Please verify your account before logging in');
-          }
-          const isPasswordCorrect = await bcrypt.compare(
-            credentials.password,
-            user.password
-          );
-          if (isPasswordCorrect) {
-            return user;
-          } else {
-            throw new Error('Incorrect password');
-          }
-        } catch (err: any) {
-          throw new Error(err);
+        },
+      }),
+    ],
+    callbacks: {
+      async jwt({ token, user }) {
+        if (user) {
+          token._id = user._id?.toString(); // Convert ObjectId to string
+          token.isVerified = user.isVerified;
+          token.isAcceptingMessages = user.isAcceptingMessages;
+          token.username = user.username;
         }
+        return token;
       },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token._id = user._id?.toString(); // Convert ObjectId to string
-        token.isVerified = user.isVerified;
-        token.isAcceptingMessages = user.isAcceptingMessages;
-        token.username = user.username;
-      }
-      return token;
+      async session({ session, token }) {
+        if (token) {
+          session.user._id = token._id;
+          session.user.isVerified = token.isVerified;
+          session.user.isAcceptingMessages = token.isAcceptingMessages;
+          session.user.username = token.username;
+        }
+        return session;
+      },
     },
-    async session({ session, token }) {
-      if (token) {
-        session.user._id = token._id;
-        session.user.isVerified = token.isVerified;
-        session.user.isAcceptingMessages = token.isAcceptingMessages;
-        session.user.username = token.username;
-      }
-      return session;
+    session: {
+      strategy: 'jwt',
     },
-  },
-  session: {
-    strategy: 'jwt',
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: '/sign-in',
-  },
+    secret: process.env.NEXTAUTH_SECRET,
+    pages: {
+      signIn: '/sign-in',
+    },
 };
